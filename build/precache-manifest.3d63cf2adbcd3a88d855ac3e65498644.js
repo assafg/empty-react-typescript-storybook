a770fea20db1babe29a5b7e82002f6cae77d7147
Assaf Gannon <assaf.gannon@gmai.com>
2019-12-07 21:06:29 +0200

    pages

---
+++ build/precache-manifest.3d63cf2adbcd3a88d855ac3e65498644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e996b943f38bbf3c05a964433b8f5b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d306390f57ab05d09ee",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "0ada8e5a457e771659b0",
+    "url": "/static/js/2.07b7175d.chunk.js"
+  },
+  {
+    "revision": "0d306390f57ab05d09ee",
+    "url": "/static/js/main.7f9718f1.chunk.js"
+  },
+  {
+    "revision": "6a578915fcf5cde07aca",
+    "url": "/static/js/runtime-main.bfe8480c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
