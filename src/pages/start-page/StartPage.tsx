9b1f3f847da0f16d43e5014524ad2e0a4dc6a9ea
Assaf Gannon <assaf.gannon@walkme.com>
2020-06-16 12:40:00 +0300

    update

---
+++ src/pages/start-page/StartPage.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function StartPage({}: Props): ReactElement {
+    return (
+        <div>
+            Home Page
+        </div>
+    )
+}
